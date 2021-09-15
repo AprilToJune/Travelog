41d2ff67d6a290f90aa6f7c088b7540262cb29e3
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-09-15 21:48:09 +0900

    파이어베이스 로딩중 표시

--- src/components/home/list/index.jsx
+++ src/components/home/list/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import List from 'components/home/list/List';
 import Modal from 'components/modal';
@@ -12,36 +13,66 @@ const Container = styled.div`
   align-items: center;
   width: 36vw;
   height: 100vh;
-  background-color: rgba(0, 0, 0, 0.1);
+  background-color: rgba(0, 0, 0, 0.01);
 `;
 
 const ListContainer = styled.div`
+  position: relative;
   width: 100%;
   height: 100%;
   background-color: rgba(255, 255, 255, 0.5);
   overflow-y: scroll;
 `;
 
+const ProgressContainer = styled.div`
+  position: absolute;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const LoadingText = styled.span`
+  position: absolute;
+  top: 55%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+
 const ExperienceListContainer = () => {
-  const { experiences } = useExperienceContext();
+  const { experiences, isDataLoading } = useExperienceContext();
 
   return (
     <Container>
       <ListContainer>
-        {experiences.map((experience) => {
-          const { id, title, startDate, endDate, location, images } = experience;
-          return (
-            <List
-              key={id}
-              id={id}
-              title={title}
-              startDate={startDate}
-              endDate={endDate}
-              location={location}
-              images={images}
-            />
-          )
-        })}
+        {isDataLoading 
+        ? (
+          <>
+            <ProgressContainer>
+              <CircularProgress />
+            </ProgressContainer>
+            <LoadingText>
+              우리의 추억을 불러오는중...
+            </LoadingText>
+          </>
+        ) 
+        : (
+          experiences.map((experience) => {
+            const { id, title, startDate, endDate, location, images } = experience;
+            return (
+              <List
+                key={id}
+                id={id}
+                title={title}
+                startDate={startDate}
+                endDate={endDate}
+                location={location}
+                images={images}
+              />
+            );
+          })
+        )
+      }
         <Modal />
       </ListContainer>
     </Container>
